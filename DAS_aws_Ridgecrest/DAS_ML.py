5f6234e90b931cc9676f391c263608e6c0da86a6
Ettore Biondi <ebiondi@caltech.edu>
2024-07-07 13:40:16 -0700

    Added magnitude estimation to tutorial

--- DAS_aws_Ridgecrest/DAS_ML.py
+++ DAS_aws_Ridgecrest/DAS_ML.py
@@ -12,6 +12,7 @@ from typing import List, Optional, Union
 import torch
 import pandas as pd
 import numpy as np
+import utm
 
 @dataclass
 class Config():
@@ -134,3 +135,58 @@ def phasenet_das(das_data, timestamp, ev_id, dt):
     with torch.cuda.device(args.device):
         torch.cuda.empty_cache()
     return picks
+
+
+def extract_peak_amp(data,TT,fs,ot,f1,f2, chIDs=None, align_wind=1.0):
+    """Function to compute SNR for a given earthquake"""
+    maxAmp = np.zeros_like(TT)
+    nch = data.shape[0] if chIDs is None else chIDs.shape[0]
+    nt = data.shape[1]
+    dt = 1.0/fs
+    align_wind= align_wind + 1.0/(f1+f2)
+    if chIDs is None:
+        chIDs = range(nch)
+    for ich in range(nch):
+        ch = chIDs[ich]
+        min_t_align = TT[ich]-align_wind*0.5
+        max_t_align = TT[ich]+align_wind*0.5
+        it_min_wind = max(0,int((min_t_align-ot)/dt+0.5))
+        it_max_wind = min(nt-1,int((max_t_align-ot)/dt+0.5))
+        maxAmp[ich] = abs(data[ch,it_min_wind:it_max_wind]).max()
+    return maxAmp
+
+
+def estimate_magnitude(StrainRate_P, StrainRate_S, ch_P, ch_S, eqLat, eqLon, arrayLat, arrayLon, site_term_P, site_term_S, M_coeff=[0.437,0.690], D_coeff=[1.269,1.588]):
+    """
+        log10(Ep) = 0.437*M - 1.269*log10(D) + Kp
+        log10(Es) = 0.690*M - 1.588*log10(D) + Ks
+    """
+    arrayX, arrayY, zoneN, zoneL  = utm.from_latlon(arrayLat,arrayLon)
+    eqX, eqY, _, _ = utm.from_latlon(eqLat,eqLon,zoneN,zoneL)
+    dist = np.sqrt((arrayX-eqX)**2+(arrayY-eqY)**2)*1e-3
+    if len(ch_P) > 0:
+        magnitudeP = -np.mean(site_term_P[ch_P] - np.log10(StrainRate_P) - D_coeff[0]*np.log10(dist[ch_P]))/M_coeff[0]
+        std_magP = np.std(site_term_P[ch_P] - np.log10(StrainRate_P) - D_coeff[0]*np.log10(dist[ch_P]))/M_coeff[0]
+    else:
+        magnitudeP = -np.inf
+        std_magP = np.inf
+    if len(ch_S) > 0:
+        magnitudeS = -np.mean(site_term_S[ch_S] - np.log10(StrainRate_S) - D_coeff[1]*np.log10(dist[ch_S]))/M_coeff[1]
+        std_magS = np.std(site_term_S[ch_S] - np.log10(StrainRate_S) - D_coeff[1]*np.log10(dist[ch_S]))/M_coeff[1]
+    else:
+        magnitudeS = -np.inf
+        std_magS = np.inf
+    # Magnitude estimation
+    if magnitudeP > -np.inf and magnitudeS > -np.inf:
+        magnitude = 0.5*(magnitudeP+magnitudeS)
+        std_mag =  0.5*(std_magP+std_magS)
+    elif magnitudeP > -np.inf and magnitudeS == -np.inf:
+        magnitude = magnitudeP
+        std_mag = std_magP
+    elif magnitudeP == -np.inf and magnitudeS > -np.inf:
+        magnitude = magnitudeS
+        std_mag = std_magS
+    else:
+        magnitude = -np.inf
+        std_mag = np.inf
+    return magnitude, std_mag
